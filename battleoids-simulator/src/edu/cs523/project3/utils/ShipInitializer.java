409985165f80257c57e47ff4c106985f9430ee2f
srbdev <me@srbdev.com>
2013-04-14 17:23:07 -0600

    Added ship initializer class (still needs mutation).

---
+++ battleoids-simulator/src/edu/cs523/project3/utils/ShipInitializer.java
@@ -0,0 +1,30 @@
+package edu.cs523.project3.utils;
+
+import edu.cs523.project3.model.Sensor;
+import edu.cs523.project3.model.Ship;
+
+public class ShipInitializer 
+{
+	private final double SENSOR_ON_PROB = 0.35; // Probability that a sensor is initialize to ON
+	
+	public ShipInitializer() {}
+	
+	public Ship initialize(Ship ship)
+	{
+		for (Sensor sensor : ship.getSensors())
+		{
+			boolean set = Math.random() > SENSOR_ON_PROB ? false : true;
+			
+			sensor.setActive(set);
+			
+			mutate(sensor);
+		}
+		
+		return ship;
+	}
+
+	private void mutate(Sensor sensor) 
+	{
+		
+	}
+}
