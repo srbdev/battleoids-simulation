770ec15d1a544fd9365a2624d7626e8fd33bd9af
srbdev <me@srbdev.com>
2013-04-15 09:38:26 -0600

    Misunderstood the initialize part so redoing it just for my GA.

---
+++ battleoids-simulator/src/edu/cs523/project3/ga/island/IslandShipInitializer.java
@@ -0,0 +1,11 @@
+package edu.cs523.project3.ga.island;
+
+
+public class IslandShipInitializer 
+{
+	private final double SENSOR_ON_PROB = 0.35; // Probability that a sensor is initialize to ON
+	
+	public IslandShipInitializer() {}
+	
+	
+}
