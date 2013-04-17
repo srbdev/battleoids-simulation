1235f382cf2cbe12b2c920f77ab75a7df7bcfac3
srbdev <me@srbdev.com>
2013-04-16 22:01:28 -0600

    Break time!

--- battleoids-simulator/src/edu/cs523/project3/ga/island/RunIslandGA.java
+++ battleoids-simulator/src/edu/cs523/project3/ga/island/RunIslandGA.java
@@ -2,6 +2,14 @@ package edu.cs523.project3.ga.island;
 
 public class RunIslandGA {
 
+	private final int N_GENERATIONS = 100;
+	private final int N_RUNS 		= 4000;
+	private final int N_ISLANDS 	= 4;
+	private final int N_MIGRATION 	= 1;
+	
+	private IslandGA ga1;
+	private IslandGA ga2;
+	
 	public static void main(String[] args) 
 	{
 
